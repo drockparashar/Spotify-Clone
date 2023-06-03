98e8c3ab0fbc62af3d6d80f37765634f24fba56a
drockparashar <pranshu007parashar@gmail.com>
2023-06-03 05:46:16 +0530

    Fixed play next feature

--- script.js
+++ script.js
@@ -2,24 +2,34 @@ let songIndex=1;
 let audioElement= new Audio("songs/"+songIndex+".mp3")
 let masterPlay= document.getElementById("masterplay")
 let progress=document.getElementById("progress-el")
-let next=document.getElementById("forward")
-let previous=document.getElementById("backward")
+let previous=document.getElementById("ahead")
+let next=document.getElementById("back")
+// console.log(next,previous,masterPlay)
 let songItem=Array.from(document.getElementsByClassName("design"))
-
-
+let songInfo=document.querySelector("songInfo")
 
 let songList=[
-    {songName:"Warriyo-Mortals[NCS Release]", songPath:"songs/1.mp3", coverPath:"covers/1.jpg"},
-    {songName:"Cielo - Huma-Huma", songPath:"songs/2.mp3", coverPath:"covers/2.jpg"},
-    {songName:"DEAF KEV - Invincible [NCS Release]", songPath:"songs/3.mp3", coverPath:"covers/3.jpg"},
-    {songName:"Different Heaven & EH!DE - My Heart [NCS Release]", songPath:"songs/4.mp3", coverPath:"covers/4.jpg"},
-    {songName:"Janji - Heroes Tonight [NCS Release]", songPath:"songs/5.mp3", coverPath:"covers/5.jpg"},
-    {songName:"Warriyo-Mortals[NCS Release]", songPath:"songs/6.mp3", coverPath:"covers/6.jpg"},
-    {songName:"Warriyo-Mortals[NCS Release]", songPath:"songs/7.mp3", coverPath:"covers/7.jpg"},
-    {songName:"Warriyo-Mortals[NCS Release]", songPath:"songs/8.mp3", coverPath:"covers/8.jpg"},
-    {songName:"Warriyo-Mortals[NCS Release]", songPath:"songs/9.mp3", coverPath:"covers/8.jpg"},
-    {songName:"Warriyo-Mortals[NCS Release]", songPath:"songs/10.mp3", coverPath:"covers/10.jpg"},
+    {songName:"Never Fold", songPath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"Mehrma", songPath:"songs/2.mp3", coverPath:"covers/2.jpg"},
+    {songName:"Chawal", songPath:"songs/3.mp3", coverPath:"covers/3.jpg"},
+    {songName:"Kajra Re", songPath:"songs/4.mp3", coverPath:"covers/4.jpg"},
+    {songName:"Malang Sajna", songPath:"songs/5.mp3", coverPath:"covers/5.jpg"},
+    {songName:"Downers At Dusk", songPath:"songs/6.mp3", coverPath:"covers/6.jpg"},
+    {songName:"Spain", songPath:"songs/7.mp3", coverPath:"covers/7.jpg"},
+    {songName:"Mi Amor", songPath:"songs/8.mp3", coverPath:"covers/8.jpg"},
+    {songName:"Cupid (Twin Version)", songPath:"songs/9.mp3", coverPath:"covers/8.jpg"},
+    {songName:"Sadi Gali", songPath:"songs/10.mp3", coverPath:"covers/10.jpg"},
 ]
+
+function plpau(n1)
+{
+    songIndex=n1
+    makeAllPlays()
+    let en1=document.getElementById(n1.toString())
+    en1.classList.remove("fa-circle-play")
+    en1.classList.add("fa-circle-pause")
+
+}
 
 songItem.forEach((element,i) => {
     element.getElementsByTagName("img")[0].src=songList[i].coverPath
@@ -33,14 +43,21 @@ masterPlay.addEventListener("click", ()=>
         audioElement.play()
         masterPlay.classList.remove("fa-circle-play")
         masterPlay.classList.add("fa-circle-pause")
+        let mp=document.getElementById(songIndex)
+        mp.classList.remove("fa-circle-pause","fa-circle-play")
+        mp.classList.add("fa-circle-pause")
     }
     else
     {
         audioElement.pause()
         masterPlay.classList.remove("fa-circle-pause")
         masterPlay.classList.add("fa-circle-play")
+        let mp=document.getElementById(songIndex)
+        mp.classList.remove("fa-circle-pause","fa-circle-play")
+        mp.classList.add("fa-circle-play")
     }
-})
+    }
+)
 
 audioElement.addEventListener("timeupdate",()=>
 {
@@ -65,35 +82,51 @@ const makeAllPlays= ()=>
 Array.from(document.getElementsByClassName("play")).forEach((element)=>
     element.addEventListener("click",(e)=>
     {
-        console.log(e)
         makeAllPlays()
         e.target.classList.remove("fa-circle-play")
         e.target.classList.add("fa-circle-pause")
-        let songId=e.target.id
-        console.log(songId)
-        audioElement.src="songs/"+songId+".mp3"
+        songIndex=e.target.id
+        audioElement.src="songs/"+songIndex+".mp3"
         audioElement.currentTime=0
         audioElement.play()
         masterPlay.classList.remove("fa-circle-play")
         masterPlay.classList.add("fa-circle-pause")
+        // songInfo.textContent=songList.songName[songIndex]
     }
 
     )
 )
-// next.addEventListener("click",()=>
-// {
-//     console.log("HII")
-//     if(songIndex<=10)
-//     {
-//         songIndex+=1
-//         audioElement.duration=0
-//         audioElement.play()
-        
-//     }
-//     else
-//     {
-//         songIndex=1
-//         audioElement.duration=0
-//         audioElement.play()
-//     }
-// })
+
+next.addEventListener("click",()=>
+{
+    if(songIndex<=10)
+        songIndex+=1
+
+    else
+        songIndex=1
+
+    plpau(songIndex)
+    audioElement.src="songs/"+songIndex+".mp3"
+    audioElement.currentTime=0
+    audioElement.play()
+    masterPlay.classList.remove("fa-circle-play")
+    masterPlay.classList.add("fa-circle-pause")
+    // songInfo.textContent=songList.songName[songIndex]
+})
+
+previous.addEventListener("click",()=>
+{
+    if(songIndex>1)
+        songIndex-=1
+
+    else
+        songIndex=10
+
+    plpau(songIndex)
+    audioElement.src="songs/"+songIndex+".mp3"
+    audioElement.currentTime=0
+    audioElement.play()
+    masterPlay.classList.remove("fa-circle-play")
+    masterPlay.classList.add("fa-circle-pause")
+    // songInfo.textContent=songList.songName[songIndex]
+})
